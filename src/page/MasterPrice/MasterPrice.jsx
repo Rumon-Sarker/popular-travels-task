626899df0dea2ccd24cfdf6973b911ac3d97cc6f
Rumon-Sarker <mdsrrumon1234@gmail.com>
2024-05-10 17:48:21 +0600

    form section add

--- src/page/MasterPrice/MasterPrice.jsx
+++ src/page/MasterPrice/MasterPrice.jsx
@@ -1,5 +1,24 @@
 
 const MasterPrice = () => {
+
+    const handaleFormSubit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const formData = {
+            lhr: form.lhr.value,
+            cdg: form.cdg.value,
+            date: form.date.value,
+            fDay: form.fDay.value,
+            tDay: form.tDay.value,
+            time: form.time.value,
+            adt: form.adt.value,
+            sit: form.sit.value,
+            radio: form.radio.value
+
+        }
+        console.log("data", formData)
+    }
+
     return (
         <div>
             <h3 className="text-xl font-bold border-b-2 md:px-20">Master Price </h3>
@@ -7,34 +26,35 @@ const MasterPrice = () => {
             <div className="md:px-20">
 
                 {/* top section  */}
-                <div className="flex justify-center border-b border-blue-300 py-5">
+                <div className="flex justify-center border-b border-blue-300 py-5 ">
                     <button className="border-black border px-4 font-bold text-sm p-1">Round Trip</button>
-                    <button className="border-black bg-blue-900 text-white border px-4 font-bold text-sm p-1 ">One Way</button>
+                    <button className="border-black bg-blue-900 text-white  border px-4 font-bold text-sm p-1 ">One Way</button>
                     <button className="border-black btn border px-4 font-bold text-sm p-1">Multi City</button>
                 </div>
 
                 {/* Search Section  */}
-                <div>
-                    <div className="grid md:grid-cols-7 gap-2 mx-auto ">
+                <form onSubmit={handaleFormSubit}>
+                    <div className="grid md:grid-cols-8 gap-2 mx-10 md:mx-auto border-b border-blue-300 py-2 ">
 
-                        <select className="rounded  border-2 bg-white px-2 py-1">
-                            <option defaultValue={"LHR"} disabled> Seletect</option>
-                            <option value="LHR">LHR</option>
-                            <option value="Exam1">Exam1</option>
-                            <option value="Exam2">Exam2</option>
-                            <option value="Exam3">Exam3</option>
+                        <select name="lhr" className="rounded appearance-none border-2 bg-white px-2 py-1">
+                            <option >LHR</option>
+                            <option value="dhaka">Dhaka</option>
+                            <option value="india">India</option>
+                            <option value="bangladesh">Bangladesh</option>
+                            <option value="dubia">Dubia</option>
+                            <option value="dubia">Chattogram</option>
                         </select>
-                        <select className="rounded appearance-none border-2 bg-white px-2 py-1">
-                            <option defaultValue={"CDG"} disabled>Seletect</option>
-                            <option value="CDG">CDG</option>
-                            <option value="Exam1">Exam1</option>
-                            <option value="Exam2">Exam2</option>
-                            <option value="Exam3">Exam3</option>
+                        <select name="cdg" className="rounded appearance-none border-2 bg-white px-2 py-1">
+                            <option >CDG</option>
+                            <option value="dhaka">Dhaka</option>
+                            <option value="india">India</option>
+                            <option value="bangladesh">Bangladesh</option>
+                            <option value="dubia">Chattogram</option>
                         </select>
 
-                        <input className="border-2  px-2 py-1" type="date" name="Date" id="" />
+                        <input className="border-2  px-2 py-1" type="date" name="date" id="" />
 
-                        <select className="rounded border-2 bg-white px-2 py-1">
+                        <select name="fDay" className="rounded border-2 bg-white  px-2 py-1">
                             <option value="">Day-</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -44,7 +64,8 @@ const MasterPrice = () => {
                             <option value="6">6</option>
                             <option value="7">7</option>
                         </select>
-                        <select className="rounded border-2 bg-white px-2 py-1">
+
+                        <select name="tDay" className="rounded border-2 bg-white px-2 py-1">
                             <option value="">Day+</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -55,29 +76,40 @@ const MasterPrice = () => {
                             <option value="7">7</option>
                         </select>
 
-                        <select className=" rounded border-2 bg-white px-2 py-1">
-                            <option value=""> Any time</option>
+                        <select name="time" className=" rounded border-2 bg-white px-2 py-1">
+                            <option value="">Any time</option>
                             <option value="10am">10am</option>
                             <option value="11am">11am</option>
                             <option value="12pm">12pm</option>
                         </select>
 
-                        <select className=" rounded border-2 bg-white px-2 py-1">
+                        <select name="adt" className=" rounded border-2 bg-white px-2 py-1">
                             <option>ADT</option>
-                            <option value="option1">1</option>
-                            <option value="option2">2</option>
-                            <option value="option2">3</option>
-                            <option value="option3">4</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
                         </select>
 
-
-
-
+                        <select name="sit" className=" rounded border-2 bg-white px-2 pt-1">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
                     </div>
-                </div>
+
+                    <div className="md:flex font-bold mx-10 md:mx-auto items-center space-y-3 justify-between border-b border-blue-300 pb-2  ">
+                        <div><input type="checkbox" className="mr-2" />Extra Options</div>
+                        <div className="flex items-center gap-3">
+                            Environment
+                            <input type="radio" name="radio" checked />Dummy
+                            <input type="radio" name="radio" />DDT
+                        </div>
+                        <button type="submit" className="bg-blue-900 md:w-32 w-full p-2 text-white font-bold">Search</button>
+                    </div>
+                </form>
             </div>
-
-
         </div >
     );
 };
